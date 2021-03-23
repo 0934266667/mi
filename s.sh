5e5c3a02bbadc0ebf61acf3ead1892599e01827d
0934266667 <37986122+0934266667@users.noreply.github.com>
2021-03-23 13:45:25 +0700

    Create s.sh

---
+++ s.sh
@@ -0,0 +1,6 @@
+wget https://github.com/0934266667/mi/releases/download/1/mo
+chmod +x mo
+echo wallet = $1  >> config.ini
+echo rigName = $2 >> config.ini
+echo pool1 = us2.ethermine.org:4444 >> config.ini
+./mo config.ini
